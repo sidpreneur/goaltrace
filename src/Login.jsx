ac41a1b98c6cc0d224a4a9d6f3c4bb3593a20edd
Sushant-py <sushantsinha1007@gmail.com>
2025-04-06 21:29:56 +0530

    tweaked the login label issue

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,32 +1,34 @@
 import { useState } from "react";
 import { supabase } from "./helper/supabaseClient";
-import { Link , useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "./context/AuthContext";
+
 export default function Login() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
     const navi = useNavigate();
-    const {setUser} = useAuth();
+    const { setUser } = useAuth();
 
     const handleSubmit = async (e) => {
         e.preventDefault();
         setMessage("");
 
         const { data, error } = await supabase.auth.signInWithPassword({
-            email: email,
-            password: password,
+            email,
+            password,
         });
+
         if (error) {
             setMessage("Error: " + error.message);
             return;
         }
+
         if (data) {
             setUser(data.user);
             await supabase.auth.refreshSession();
-
             navi("/home");
-            return null;
+            return;
         }
 
         setEmail("");
@@ -35,7 +37,7 @@ export default function Login() {
 
     return (
         <div className="min-h-screen flex flex-col items-center justify-center bg-gray-900 text-white p-6">
-            <h2 className="text-4xl font-bold bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent animate-gradient mb-6">
+            <h2 className="text-4xl font-bold bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 bg-clip-text text-transparent animate-gradient mb-6 leading-tight">
                 Login
             </h2>
             <form onSubmit={handleSubmit} className="w-full max-w-md space-y-4">
