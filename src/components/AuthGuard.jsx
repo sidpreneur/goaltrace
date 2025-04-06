4a3ec9fcf6f19eb45fedf6791acf4b6ac988cd36
sidpreneur <sidpreneur@gmail.com>
2025-04-06 23:06:16 +0530

    Not login on refresh

--- src/components/AuthGuard.jsx
+++ src/components/AuthGuard.jsx
@@ -2,7 +2,11 @@ import { useAuth } from "../context/AuthContext";
 import { Navigate } from "react-router-dom";
 
 export default function AuthGuard({ children }) {
-    const { user } = useAuth();
+    const { user, loading } = useAuth();
+
+    if (loading) {
+        return null; // Or a spinner like <LoadingScreen />
+    }
 
     if (!user) {
         return <Navigate to="/login" />;
